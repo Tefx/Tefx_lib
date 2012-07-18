de43cb7f5a673cc974b200b1ce4327218a807931
Zhu Zhaomeng <Zhaomeng.Zhu@Gmail.com>
2012-07-18 13:45:53 +0800

    add client for netable:

--- py/netable.py
+++ py/netable.py
@@ -13,6 +13,22 @@ class Netable(server.MessageServer):
         port.send(res)
         port.close()
 
+
+class Client(object):
+    def __init__(self, adderss):
+        self.server_address = adderss
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect(adderss)
+        self.port = MessagePort(sock.makefile())
+
+    def call(self, func, args):
+        self.port.send((func, args))
+        return self.port.recv()
+
+    def shutdown(self):
+        if self.port:
+            self.port.close()
+
 if __name__ == '__main__':
     class Test(object):
         def add(self, a, b):
